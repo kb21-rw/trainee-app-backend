ddbb64c916d457174b75717bd00e8c5e86f2616f
David Gusenga <gsngdavid@gmail.com>
2024-11-06 16:35:48 +0200

    responde with an appropriate error

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,4 +1,4 @@
-import { Response } from "express";
+import { NextFunction, Response } from "express";
 import { verify } from "jsonwebtoken";
 import dotenv from "dotenv";
 import { getUserService } from "../services/userService";
@@ -7,7 +7,7 @@ dotenv.config();
 
 const accessKey = process.env.ACCESS_TOKEN_KEY || "";
 
-export const verifyJWT = (req: any, res: Response, next: () => void) => {
+export const verifyJWT = (req: any, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
   if (!authHeader?.startsWith("Bearer ")) {
     return res.status(401).send("User not logged in");
@@ -16,11 +16,13 @@ export const verifyJWT = (req: any, res: Response, next: () => void) => {
   const token = authHeader.split(" ")[1];
   verify(token, accessKey, async (err: any, decoded: any) => {
     if (err) {
-      return res.status(403).send("access token not valid");
+      return res.status(403).json({
+        type: "JsonWebTokenError",
+        errorMessage: "Invalid access token",
+      });
     }
 
-    
-    req.user = await getUserService({_id: decoded.id});
+    req.user = await getUserService({ _id: decoded.id });
     next();
   });
 };
