d4d743e5d035f7c8ad42cccaa1bfc2bb0174ff02
gsngdavid <gsngdavid@gmail.com>
2024-12-05 11:34:43 +0200

    refactor: get coaches

--- src/routes/coachRoute.ts
+++ src/routes/coachRoute.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { addCoachToCohortController, getCoaches, updateCoachOrAdmin } from "../controllers/coachController";
+import { addCoachToCohortController, getCoachesController, updateCoachOrAdmin } from "../controllers/coachController";
 import { verifyJWT } from "../middlewares/authenticate";
 import { isAdmin } from "../middlewares/authorization";
 
 const router = Router();
 
-router.get("/all", verifyJWT, getCoaches);
+router.get("/", verifyJWT, isAdmin, getCoachesController);
 router.patch("/edit-coach-or-admin/:id", verifyJWT, updateCoachOrAdmin);
 router.post("/", verifyJWT, isAdmin, addCoachToCohortController);
 
