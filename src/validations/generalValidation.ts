e464f3cc182c277e22a60341926229e8446b1e81
David Gusenga <gsngdavid@gmail.com>
2024-12-12 17:05:37 +0200

    feat: add required mongodbId validation

--- src/validations/generalValidation.ts
+++ src/validations/generalValidation.ts
@@ -4,3 +4,9 @@ export const mongodbIdValidation = Joi.string()
   .hex()
   .length(24)
   .message("Invalid document Id");
+
+export const requiredMongodbIdValidation = Joi.string()
+  .hex()
+  .length(24)
+  .message("Invalid document Id")
+  .required();
