54c70b5a73c339c79d96cafefeb04b271aec2abd
Rebakure <kevinrebakure@gmail.com>
2025-03-17 18:57:47 +0200

    feat: create joinWaitListController

---
+++ src/controllers/joinWaitListController.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express"
+import { joinWaitListService } from "../services/joinWaitListService"
+import { joinWaitListFormValidation } from "../validations/joinWaitListValidation"
+
+export const joinWaitListController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    await joinWaitListFormValidation.validateAsync(req.body)
+
+    const joinWaitListForm = await joinWaitListService(req.body)
+
+    return res.status(201).json({
+      message: "You've been added to the waitlist!",
+      user: joinWaitListForm,
+    })
+  } catch (error) {
+    return next(error)
+  }
+}
