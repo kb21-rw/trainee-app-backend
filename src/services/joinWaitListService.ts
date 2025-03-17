e2fcd74f9401f4d09153f275bf3502b35c03a51c
Rebakure <kevinrebakure@gmail.com>
2025-03-17 18:59:42 +0200

    feat: create join wait list service

---
+++ src/services/joinWaitListService.ts
@@ -0,0 +1,23 @@
+import CustomError from "../middlewares/customError"
+import { getProspect } from "../queries/prospectQuery"
+import { addProspectToTheWaitList } from "../queries/waitListQuery"
+import { USER_NOT_FOUND } from "../utils/errorCodes"
+import { JoinWaitListDto } from "../utils/types"
+
+const joinWaitListService = async (joinWaitListData: JoinWaitListDto) => {
+  const email = joinWaitListData.responses.email
+  const prospect = await getProspect(email)
+
+  if (prospect) {
+    const addedProspect = await addProspectToTheWaitList(email)
+    return addedProspect
+  }
+
+  throw new CustomError(
+    USER_NOT_FOUND,
+    "Provide the email you used for signing up on this app!",
+    500,
+  )
+}
+
+export { joinWaitListService }
