6c90e8cdeda71847f4eb889440b84065a4d290e2
Rebakure <kevinrebakure@gmail.com>
2025-03-24 18:53:43 +0200

    feat: error handling in joinwaitlist service

--- src/services/joinWaitListService.ts
+++ src/services/joinWaitListService.ts
@@ -1,27 +1,34 @@
 import CustomError from "../middlewares/customError"
 import { getProspect } from "../queries/prospectQuery"
 import { addProspectToTheWaitList } from "../queries/waitListQuery"
-import { io } from "../server"
+import { io, room } from "../server"
 import { USER_NOT_FOUND } from "../utils/errorCodes"
 import { JoinWaitListDto } from "../utils/types"
 
 const joinWaitListService = async (joinWaitListData: JoinWaitListDto) => {
+  const joinRoom = room[0]
   const email = joinWaitListData.responses.email
+
   const prospect = await getProspect(email)
 
-  if (prospect) {
+  if (prospect && !prospect.isOnWaitList) {
     const addedProspect = await addProspectToTheWaitList(email)
 
-    io.to(email).emit("joinedTheWaitList", { email })
+    io.to(joinRoom).emit("joinedTheWaitList", { email })
 
     return addedProspect
-  }
+  } else {
+    io.to(joinRoom).emit("waitListError", {
+      errorMessage:
+        "Provide the email you used while registering into the app!",
+    })
 
-  throw new CustomError(
-    USER_NOT_FOUND,
-    "Provide the email you used for signing up on this app!",
-    500,
-  )
+    throw new CustomError(
+      USER_NOT_FOUND,
+      "Provide the email you used for signing up on this app!",
+      500,
+    )
+  }
 }
 
 export { joinWaitListService }
