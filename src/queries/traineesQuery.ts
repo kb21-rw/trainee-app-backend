c8598fabb7e15e1e46a69d172d6d36db03202385
InshutiSouvede <isouvede2@gmail.com>
2025-04-28 11:32:18 +0200

    chore: remove the unused getTraineesQuery

--- src/queries/traineesQuery.ts
+++ src/queries/traineesQuery.ts
@@ -3,64 +3,6 @@ import mongoose, { Types } from "mongoose"
 import User from "../models/User"
 import { Role } from "../utils/types"
 
-export const getTraineesQuery = async (
-  searchString: string,
-  sortBy: string,
-  traineesPerPage: number,
-) => {
-  const trainees = await User.aggregate([
-    {
-      $match: {
-        $or: [{ name: { $regex: new RegExp(searchString, "i") } }],
-        role: Role.Trainee,
-      },
-    },
-    {
-      $lookup: {
-        from: "users",
-        localField: "coach",
-        foreignField: "_id",
-        as: "coach",
-      },
-    },
-    {
-      $addFields: {
-        coach: {
-          $cond: {
-            if: { $eq: [{ $size: "$coach" }, 0] },
-            then: [{}],
-            else: "$coach",
-          },
-        },
-      },
-    },
-    {
-      $project: {
-        _id: 1,
-        name: 1,
-        email: 1,
-        role: 1,
-        coach: {
-          $cond: {
-            if: { $eq: [{ $size: "$coach" }, 0] },
-            then: {},
-            else: {
-              $arrayElemAt: ["$coach", 0],
-            },
-          },
-        },
-      },
-    },
-    {
-      $sort: { [sortBy]: 1 },
-    },
-    {
-      $limit: traineesPerPage,
-    },
-  ])
-  return trainees
-}
-
 export const getTraineesWithDetailsQuery = async (
   cohortId: string,
   sortBy: string,
