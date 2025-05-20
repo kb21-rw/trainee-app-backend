1570c381c1aee1d99049613020d3ac774eb9a4b6
InshutiSouvede <isouvede2@gmail.com>
2025-05-20 13:24:07 +0200

    fix: revert the changes on dev and fix the route for get all trainees

--- src/controllers/traineeController.ts
+++ src/controllers/traineeController.ts
@@ -12,7 +12,7 @@ export const getTrainees = async (
   next: NextFunction,
 ) => {
   const searchString = String(req.query.searchString || "")
-  const traineesPerPage = Number(req.query.coachesPerPage) || 10
+  const traineesPerPage = Number(req.query.traineesPerPage) || 10
   const sortBy = String(req.query.sortBy || "entry")
   try {
     const trainees = await getTraineesService({
