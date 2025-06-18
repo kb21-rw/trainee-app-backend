cfd72ca874ce39355e5bbc3aa20a535b9e19bd45
Rebakure <kevinrebakure@gmail.com>
2025-06-18 12:28:31 +0200

    feat: add newAuthRoute and newUserRoute

---
+++ src/routes/newAuthRoute.ts
@@ -0,0 +1,25 @@
+import { Router } from "express"
+import {
+  applicantRegister,
+  googleAuthController,
+  login,
+  register,
+  resetPassword,
+  verifyApplicant,
+} from "../controllers/newAuthController"
+import { verifyJWT } from "../middlewares/authenticate"
+import { isAdmin } from "../middlewares/authorization"
+
+const router = Router()
+
+router.post("/register", verifyJWT, isAdmin, register)
+router.post("/login", login)
+router.post("/reset-password", resetPassword)
+
+// Applicant
+router.post("/register/applicant", applicantRegister)
+router.patch("/applicant/verify", verifyApplicant)
+
+router.post("/google", googleAuthController)
+
+export default router
