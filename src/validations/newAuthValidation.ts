89ffd7536689b73fad621c33e4a58de917813439
Rebakure <kevinrebakure@gmail.com>
2025-06-18 12:29:31 +0200

    feat: add newAuthValidation and newUserValidation

---
+++ src/validations/newAuthValidation.ts
@@ -0,0 +1,29 @@
+import Joi from "joi"
+import { NewRole } from "../utils/types"
+
+export const registerSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  role: Joi.string()
+    .valid(NewRole.Admin, NewRole.Coach, NewRole.Trainee)
+    .required(),
+})
+
+export const applicantRegisterSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string()
+    .regex(
+      /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8,}$/, // Check for strong password required (Capital/Small letter, special character and a number)
+    )
+    .required(),
+})
+
+export const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+})
+
+export const resetPasswordSchema = Joi.object({
+  email: Joi.string().email().required(),
+})
